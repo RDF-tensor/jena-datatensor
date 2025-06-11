657f71247013e481e905803e42fb8c4b4711dcab
cinekele <cinekele@gmail.com>
2025-06-11 22:56:55 +0200

    modify getter

--- jena-datatensor/src/main/java/pl/edu/pw/mini/jena/datatensor/functions/indexers/GetSubDT.java
+++ jena-datatensor/src/main/java/pl/edu/pw/mini/jena/datatensor/functions/indexers/GetSubDT.java
@@ -1,16 +1,53 @@
 package pl.edu.pw.mini.jena.datatensor.functions.indexers;
 
+import org.apache.jena.datatypes.RDFDatatype;
 import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.sparql.expr.NodeValue;
+import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import pl.edu.pw.mini.jena.datatensor.datatypes.implementations.BooleanDataTensor;
 import pl.edu.pw.mini.jena.datatensor.datatypes.implementations.NumericDataTensor;
-import pl.edu.pw.mini.jena.datatensor.functions.NumericDT2FunctionBase;
+import pl.edu.pw.mini.jena.datatensor.functions.GenericDT2FunctionBase;
 
-public class GetSubDT extends NumericDT2FunctionBase {
+public class GetSubDT extends GenericDT2FunctionBase {
+
+    public GetSubDT() {
+        super(false);
+    }
 
     @Override
     public NodeValue calc(INDArray v1, INDArray v2) {
-        INDArray result = v1.get(v2);
-        return NodeValue.makeNode(NodeFactory.createLiteralByValue(result, NumericDataTensor.INSTANCE));
+        INDArray result;
+        if (v2.dataType() == DataType.BOOL) result = booleanIndexing(v1, v2);
+        else result = v1.get(v2);
+
+        NodeValue nv;
+        if (v1.dataType().isNumerical())
+            nv = NodeValue.makeNode(NodeFactory.createLiteralByValue(result, NumericDataTensor.INSTANCE));
+        else if (v1.dataType() == DataType.BOOL)
+            nv = NodeValue.makeNode(NodeFactory.createLiteralByValue(result, BooleanDataTensor.INSTANCE));
+        else
+            throw new IllegalArgumentException("Unsupported data type for GetSubDT operation");
+        return nv;
     }
+
+    public static INDArray booleanIndexing(INDArray data, INDArray mask) {
+        data = data.ravel();
+        mask = mask.ravel();
+
+        int count = mask.sumNumber().intValue();
+
+        INDArray result = Nd4j.create(count);
+
+        int index = 0;
+        for (int i = 0; i < data.length(); i++) {
+            if (mask.getInt(i) == 1) {
+                result.putScalar(index++, data.getDouble(i));
+            }
+        }
+
+        return result.castTo(data.dataType());
+    }
+
 }
--- jena-datatensor/src/test/java/pl/edu/pw/mini/jena/datatensor/functions/indexers/TestDTIndexers.java
+++ jena-datatensor/src/test/java/pl/edu/pw/mini/jena/datatensor/functions/indexers/TestDTIndexers.java
@@ -52,4 +52,34 @@ public class TestDTIndexers {
     public void getSubDTTest8() {
         testError("dtf:getSubDT(\"{\\\"type\\\":\\\"int32\\\",\\\"shape\\\":[2,2,2],\\\"data\\\":[3,2,3,4,3,2,3,4]}\"^^dt:NumericDataTensor, \"{\\\"type\\\":\\\"int32\\\",\\\"shape\\\":[3, 1],\\\"data\\\":[2,2,2]}\"^^dt:NumericDataTensor)");
     }
+
+    @Test
+    public void getSubDTTest9() {
+        test("dtf:getSubDT(\"{\\\"type\\\":\\\"int32\\\",\\\"shape\\\":[8],\\\"data\\\":[3,2,3,4,3,2,3,4]}\"^^dt:NumericDataTensor, \"{\\\"type\\\":\\\"int32\\\",\\\"shape\\\":[2],\\\"data\\\":[0,1]}\"^^dt:NumericDataTensor)\n",
+                "\"{\\\"type\\\":\\\"int32\\\", \\\"shape\\\":[2], \\\"data\\\":[3,2]}\"^^dt:NumericDataTensor");
+    }
+
+    @Test
+    public void getSubDTTest10() {
+        test("dtf:getSubDT(\"{\\\"type\\\":\\\"int32\\\",\\\"shape\\\":[2,2],\\\"data\\\":[1,2,3,4]}\"^^dt:NumericDataTensor,\"{\\\"type\\\":\\\"int32\\\",\\\"shape\\\":[2,2],\\\"data\\\":[0,1,1,0]}\"^^dt:NumericDataTensor)",
+                "\"{\\\"type\\\":\\\"int32\\\",\\\"shape\\\":[2],\\\"data\\\":[2,3]}\"^^dt:NumericDataTensor");
+    }
+
+    @Test
+    public void getSubDTTest11() {
+        test("dtf:getSubDT(\"{\\\"type\\\":\\\"int32\\\",\\\"shape\\\":[2,2,2],\\\"data\\\":[1,2,3,4,5,6,7,8]}\"^^dt:NumericDataTensor,\"{\\\"type\\\":\\\"int32\\\",\\\"shape\\\":[1, 1],\\\"data\\\":[1]}\"^^dt:NumericDataTensor)",
+                "\"{\\\"type\\\":\\\"int32\\\",\\\"shape\\\":[2,2],\\\"data\\\":[5,6,7,8]}\"^^dt:NumericDataTensor");
+    }
+
+    @Test
+    public void getSubDTTest12() {
+        test("dtf:getSubDT(\"{\\\"type\\\":\\\"int32\\\",\\\"shape\\\":[2,2,2],\\\"data\\\":[1,2,3,4,5,6,7,8]}\"^^dt:NumericDataTensor,\"{\\\"type\\\":\\\"int32\\\",\\\"shape\\\":[2, 2],\\\"data\\\":[0, 0, 1, 1]}\"^^dt:NumericDataTensor)",
+                "\"{\\\"type\\\":\\\"int32\\\",\\\"shape\\\":[2,2],\\\"data\\\":[3, 4, 3, 4]}\"^^dt:NumericDataTensor");
+    }
+
+    @Test
+    public void getSubDTTest13() {
+        test("dtf:getSubDT(\"{\\\"type\\\":\\\"int32\\\",\\\"shape\\\":[2, 2],\\\"data\\\":[3, 2, 3, 4]}\"^^dt:NumericDataTensor, \"{\\\"shape\\\":[2, 2],\\\"data\\\":[true, false, true, true]}\"^^dt:BooleanDataTensor)",
+                "\"{\"type\": \"int32\", \"shape\": [3], \"data\": [3, 3, 4]}\"^^dt:NumericDataTensor\"");
+    }
 }
